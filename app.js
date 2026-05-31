71666e4c7d15baafe5a5bd74d297a5d56ca199ff
ezrabales <bales.ezra@gmail.com>
2026-05-30 21:26:40 -0600

    edit mongoose production connection

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const PORT = process.env.PORT || 3001;
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch(console.error);
 
 app.use(requestLogger);
 app.use(express.json());
