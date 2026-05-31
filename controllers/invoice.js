f165d5396b10f463bcc9fc7c3a3f9e0b503b0ad1
ezrabales <bales.ezra@gmail.com>
2026-05-30 23:13:14 -0600

    use const require duh

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -1,6 +1,6 @@
 const Invoice = require("../models/invoice");
 const PDFDocument = require("pdfkit");
-import { Resend } from "resend";
+const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 const Job = require("../models/job");
 
