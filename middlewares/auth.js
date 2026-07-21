f74a216872ea2ad0ccfd86a2ce820a403a2589c4
ezrabales <bales.ezra@gmail.com>
2026-07-20 19:11:14 -0600

    add a tester profile with limited functionality

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,17 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const UnauthorizedError = require("../errors/UnauthorizedError");
 
+const unauthorizedTesting = [
+  { method: "POST", baseUrl: "/invoice" },
+  { method: "PATCH", baseUrl: "/users" },
+];
+
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer ")) {
     next(new UnauthorizedError("authorization required"));
   }
+
   const token = authorization.replace("Bearer ", "");
   let payload;
 
@@ -17,6 +23,19 @@ module.exports.auth = (req, res, next) => {
   }
 
   req.user = payload;
+
+  // handle unauthorized actions while testing
+  if (req?.user?._id === "6a3562381d37c2c09dd4d234") {
+    const isUnauthorizedTest = unauthorizedTesting.some(
+      (route) => route.method === req.method && route.baseUrl === req.baseUrl,
+    );
+    if (isUnauthorizedTest) {
+      next(new UnauthorizedError("unauthorized action for testing"));
+    }
+  }
+  // console.log(req.user._id);
+  // console.log(req.baseUrl);
+  // console.log(req.method);
 
   return next();
 };
